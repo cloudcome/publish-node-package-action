6ce6bedd17b82dbe7a29b0b79f39eb1cee9c5fa2
#云淡然 <hi@ydr.me>
2024-07-23 23:56:43 +0800

    refactor(action): simplify export logic and enhance script replacement

--- scripts/replace-action.cjs
+++ scripts/replace-action.cjs
@@ -8,8 +8,9 @@ const write = (f, d) => fs.writeFileSync(resolve(f), d, 'utf8');
 const source = read('dist/index.cjs');
 const action = read('action.yml');
 const action2 = action.replace(/^(\s+)\/\/ SCRIPT REPLACE START[\s\S]*?^(\s+)\/\/ SCRIPT REPLACE END/m, (_, indent) => {
+    const source2 = source.trim().replace(/^/gm, indent);
     return `${indent}// SCRIPT REPLACE START
-${source.trim().replace(/^/gm, indent)}
+${source2}
 ${indent}// SCRIPT REPLACE END`;
 });
 
